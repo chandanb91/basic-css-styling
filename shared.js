011cdd3b60f444d99ed5e092bef7e5f5bae640cc
Chandan <chandan7791@gmail.com>
2018-12-21 12:45:05 +0530

    Opening and Closing the Hamburger Menu

--- shared.js
+++ shared.js
@@ -2,6 +2,8 @@ var backdrop = document.querySelector('.backdrop');
 var modal = document.querySelector('.modal');
 var selectPlanButtons = document.querySelectorAll('.plan button');
 var selectNoButton = document.querySelector('.modal__action--negative');
+var toggleButton = document.querySelector('.toggle-button');
+var mobileNav = document.querySelector('.mobile-nav');
 
 // console.dir(backdrop);
 // console.dir(selectPlanButtons);
@@ -16,9 +18,17 @@ for(var i = 0; i < selectPlanButtons.length ; i++) {
 
 selectNoButton.addEventListener('click', closeModal);
 
-backdrop.addEventListener('click', closeModal);
+backdrop.addEventListener('click', function() {
+    mobileNav.style.display = 'none';
+    closeModal();
+});
 
 function closeModal() {
     modal.style.display = 'none';
     backdrop.style.display = 'none';
 }
+
+toggleButton.addEventListener('click', function() {
+    mobileNav.style.display = 'block';
+    backdrop.style.display = 'block';
+});
